d7c0a9adfdbabe1864cbaa557740fec4e9c376f3
Emily Garvey <emilyg@scripted.org>
2018-11-13 18:31:19 +0000

    var

--- js/script.js
+++ js/script.js
@@ -5,10 +5,57 @@ $(document).ready(function() {
         var name = $("#name").val();
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var q1Result = $("#question1").val();
+        var q1Result = $("#question1inches").val();
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
+
+        var q1num = height(q1Result);
+        var q2num = fun(q2Result);
+        var q3num = day(q3Result);
+
+        totalScore = q1num + q2num + q3num;
+
+        $(".result").html(totalScore);
     });
+    
+function height(inches){
+    if (inches > "8") {
+        return 3;
+    } else if (inches > "3" && inches < "9") {
+        return 2; 
+    } else if (inches > "12") {
+        return 4;
+    } else {
+        return 1;
+    }
+}
+function fun(verb){
+    if (verb === "read") {
+        return 1;
+    } else if (verb === "listen") {
+        return 2;
+    } else if (verb === "talk") {
+        return 3;
+    } else if (verb === "play") {
+        return 4;
+    } else {
+        return 2;
+    }
+}
+
+function day(time) {
+    if (time === "morning") {
+        return 1;
+    } else if (time === "evening") {
+        return 2;
+    } else if (time === "night") {
+        return 3;
+    } else if (time === "afternoon") {
+        return 4;
+    } else {
+        return 2;
+    }
+}
 
 });
